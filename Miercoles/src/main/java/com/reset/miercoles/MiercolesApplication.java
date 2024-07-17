5bdb4608e6b3ef0b9e7cf772dd25ee7d87048202
dafhome <d.fernalvarez@gmail.com>
2024-07-17 19:13:38 +0200

    commit

---
+++ Miercoles/src/main/java/com/reset/miercoles/MiercolesApplication.java
@@ -0,0 +1,13 @@
+package com.reset.miercoles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiercolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiercolesApplication.class, args);
+	}
+
+}
---
+++ Miercoles/src/test/java/com/reset/miercoles/miercolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reset.miercoles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiercolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
